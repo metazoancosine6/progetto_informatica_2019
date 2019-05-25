874bd92af533a49a7b91baee278ba7af1c842396
Dag7 <44711271+dag7dev@users.noreply.github.com>
2019-05-25 20:46:27 +0200

    utilizzare questo per creare nuove pagine

---
+++ template.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="../css/style.css"  >    
+    </head>
+    <body>
+        <h1 class="center bgblack header_page"> <!-- INSERIRE NOME PAGINA QUI --></h1>
+        
+        <div class = 'navbar bgblack maxwidth' id='menu'>
+            <?php
+                include("utils.php");
+                session_start();
+                createNavBar();
+            ?>
+        </div>
+        
+        <!--
+        	corpo della pagina, tipicamente questo e' il riquadrone bianco classico al centro 
+        	ma in caso di particolari esigenze si puo' tranquillamente eliminare
+        -->
+        <div class="bgwhite" style="height: 400px; padding:5%; width: 40%; margin: 0px auto ">
+        	    
+
+        </div>
+	</body>
+</html>
