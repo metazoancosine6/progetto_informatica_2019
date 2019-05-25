3b32099f0e6af88df0d91d654598e991e0f453d3
metazoancosine6 <51028949+metazoancosine6@users.noreply.github.com>
2019-05-25 18:49:37 +0200

    Update AccessoRiuscito.php

--- pages/AccessoRiuscito.php
+++ pages/AccessoRiuscito.php
@@ -12,49 +12,17 @@ and open the template in the editor.
     </head>
     <body>
 
-        <?php session_start(); ?>
-        <div class="navbar max-widht bgblack">
-            <h1 style="border-bottom: 3px solid white;" class="center max-widht">Simauto</h1>
-            <div class="navbar-left" id="menu">
-                <a href="index.php">Home</a>
-                <?php
-                if ((!isset($_SESSION["username"]) || $_SESSION["privilegi"] == 1)) {
-                    echo "<a href='#'>Servizi</a>";
-                    echo "<a href='#'>Chi siamo</a>";
-                    echo "<a href='#'>Registrati</a>";
-                    echo" <a href='login.php'>Login</a>";
-                }
-
-                if (isset($_SESSION["username"])) {
-                    echo "<a href='profilo.php'>";
-                    if ($_SESSION["privilegi"] == 1) {
-                        echo "Utente:";
-                    } else if ($_SESSION["privilegi"] == 2) {
-                        echo "Meccanico:";
-                    } else if ($_SESSION["privilegi"] == 3) {
-                        echo "Amministratore:";
-                    } else {
-                        
-                    }
-
-
-                    echo $_SESSION["username"] . "</a>";
-                    if ($_SESSION["privilegi"] == 2) {
-                        echo "<a href='meccanicoOperazioni.php'>Operazioni meccanico</a>";
-                    } else if ($_SESSION["privilegi"] == 3) {
-
-                        echo "<a href='LicenziaMeccanico.php'>Licenzia Meccanico</a>";
-                    }
-                } else {
-                    echo " ";
-                }
+        <?php session_start(); 
+        include "utils.php";
+        createNavBar();
+        
                 ?>
 
             </div>
         </div>  
 
         <div class="bgwhite" style="height: 400px; padding:5%; width: 40%; margin: 0px auto ">
-            <h3 style="color: black">Accesso effetuato</h3>
+            <h3 style="color: black">Accesso effettuato</h3>
         </div>
     </body>
 </html>
