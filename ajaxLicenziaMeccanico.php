4b09a88bf972df6785a6e720d18d1d7813586525
Dag7 <44711271+dag7dev@users.noreply.github.com>
2019-06-01 12:16:35 +0200

    Create ajaxLicenziaMeccanico.php

---
+++ ajaxLicenziaMeccanico.php
@@ -0,0 +1,19 @@
+<?php
+
+	include("utils.php");
+	$myconn=connect();
+	//Matricola del meccanico
+	$meccanico=$_GET["meccanico"];
+	//ricerca del nome e cognome 
+	$query="SELECT nome_m, cognome_m
+			FROM meccanico 
+			WHERE matricola="."'".$meccanico."';";
+	//$ris=execute($myconn,$query);
+	$ris = $myconn -> query($query);
+	
+	$row = $ris->fetch_assoc();
+	//concatenazione del messaggio (ovvero delle casella)
+	$mex="<b>Nome :</b>  " . $row["nome_m"] . "<br>";
+	$mex=$mex."<b>Cognome :</b> " . $row["cognome_m"];
+	echo $mex;
+?>
